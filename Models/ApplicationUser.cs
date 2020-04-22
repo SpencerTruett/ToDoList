18357f1eb0d9a8cec5b5c2031e9007c78b888ab8
Spencer Truett <srtruett1993@gmail.com>
2020-04-22 13:10:23 -0500

    first commit

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace ToDoList.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public List<ToDoItem> ToDoItems { get; set; }
+
+    }
+}
